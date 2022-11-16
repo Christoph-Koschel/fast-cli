a725d187c5934339174f95ee165fb03cfde21179
Christoph Koschel <christoph.koschel@gmail.com>
2022-11-16 20:35:58 +0100

    Init commit

---
+++ src/handler.ts
@@ -0,0 +1,191 @@
+export class ArgumentHandler {
+    private flags: ArgumentFlag[];
+    private attrs: ArgumentAttribute[];
+
+    public constructor(flags: ArgumentFlag[], attrs: ArgumentAttribute[]) {
+        this.flags = flags;
+        this.attrs = attrs;
+    }
+
+    public hasFlag(name: string): boolean {
+        for (let flag of this.flags) {
+            if (flag.name == name) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public hasAttr(name: string) {
+        for (let attr of this.attrs) {
+            if (attr.name == name) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public getAttr(name: string): string | null {
+        for (let attr of this.attrs) {
+            if (attr.name == name) {
+                return attr.value;
+            }
+        }
+
+        return null;
+    }
+
+    public flagLength() {
+        return this.flags.length;
+    }
+
+    public attrLength() {
+        return this.attrs.length;
+    }
+}
+
+
+export class ArgumentFlag {
+    public readonly name: string;
+
+    public constructor(name: string) {
+        this.name = name;
+    }
+}
+
+export class ArgumentAttribute {
+    public readonly name: string;
+    public readonly value: string;
+
+    public constructor(name: string, value: string) {
+        this.name = name;
+        this.value = value;
+
+    }
+}
+
+export abstract class Command {
+    public abstract execute(argv: ArgumentHandler): number;
+
+    public abstract getCMD(): CommandConstructor;
+
+    public abstract getDescription(): string;
+}
+
+export class CommandConstructor {
+    private name: string;
+    private flags: CommandFlag[];
+    private attrs: CommandAttribute[];
+
+    public constructor(name: string) {
+        this.name = name;
+        this.flags = [];
+        this.attrs = [];
+    }
+
+    public addFlag(name: string, optional: boolean, description: string): CommandConstructor {
+        this.flags.push(new CommandFlag(name, optional, description));
+        return this;
+    }
+
+    public addAttribute(char: string, name: string, optional: boolean, description: string): CommandConstructor {
+        this.attrs.push(new CommandAttribute(char, name, optional, description));
+        return this;
+    }
+
+    public equals(name: string, argv: ArgumentHandler): boolean {
+        if (name != this.name) {
+            return false;
+        }
+
+        for (let attr of this.attrs) {
+            if (attr.optional) {
+                continue;
+            }
+
+            if (!argv.hasAttr(attr.char)) {
+                return false;
+            }
+        }
+
+        for (let flag of this.flags) {
+            if (flag.optional) {
+                continue;
+            }
+
+            if (!argv.hasFlag(flag.name)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public toString(): string {
+        let str: string = this.name + " ";
+
+        for (let attr of this.attrs) {
+            if (!attr.optional) {
+                str += attr.char + " <" + attr.name + "> ";
+            }
+        }
+
+        for (let attr of this.attrs) {
+            if (attr.optional) {
+                str += "[" + attr.char + " <" + attr.name + ">] ";
+            }
+        }
+
+        for (let flag of this.flags) {
+            if (!flag.optional) {
+                str += flag.name + " ";
+            }
+        }
+
+        for (let flag of this.flags) {
+            if (flag.optional) {
+                str += "[" + flag.name + "] ";
+            }
+        }
+
+        str += "\n\n";
+        for (let attr of this.attrs) {
+            str += attr.char + (attr.optional ? "  [OPTIONAL]" : "") + "   " + attr.description + "\n";
+        }
+
+        for (let flags of this.flags) {
+            str += flags.name + (flags.optional ? "  [OPTIONAL]" : "") + "   " + flags.description + "\n";
+        }
+
+        return str;
+    }
+}
+
+export class CommandFlag {
+    public readonly name: string;
+    public readonly optional: boolean;
+    public readonly description: string;
+
+    public constructor(name: string, optional: boolean, description: string) {
+        this.name = name;
+        this.optional = optional;
+        this.description = description;
+    }
+}
+
+export class CommandAttribute {
+    public readonly char: string;
+    public readonly name: string;
+    public readonly optional: boolean;
+    public readonly description: string;
+
+    public constructor(char: string, name: string, optional: boolean, description: string) {
+        this.char = char;
+        this.name = name;
+        this.optional = optional;
+        this.description = description;
+
+    }
+}
---
+++ src/parser.ts
@@ -0,0 +1,78 @@
+import {ArgumentAttribute, ArgumentFlag, ArgumentHandler, Command, CommandConstructor} from "./handler";
+
+export class CLI {
+    private args: string[];
+    private cmds: Command[];
+
+    public constructor(args: string[]) {
+        args.shift();
+        args.shift();
+        this.args = args;
+        this.cmds = [];
+    }
+
+    public register(cmd: Command) {
+        this.cmds.push(cmd);
+    }
+
+    public exec(): number {
+        const cmdName: string | null = this.args[0].startsWith("-") ? null : <string>this.args.shift();
+        const argumentHandler: ArgumentHandler = this.buildArguments();
+
+        const isHelp = argumentHandler.hasFlag("--help");
+
+        if (isHelp && cmdName == null && argumentHandler.attrLength() == 0 && argumentHandler.flagLength() == 1) {
+            this.printGlobalHelp();
+            return 0;
+        } else if (cmdName == null) {
+            this.printCommandNotFound();
+            return 1;
+        }
+
+        for (let cmd of this.cmds) {
+            let command: CommandConstructor = cmd.getCMD();
+            if (command.equals(cmdName, argumentHandler)) {
+                if (isHelp) {
+                    process.stdout.write(command.toString());
+                } else {
+                    return cmd.execute(argumentHandler);
+                }
+            }
+        }
+
+        this.printCommandNotFound();
+        return 1;
+    }
+
+    private buildArguments(): ArgumentHandler {
+        const attrs: ArgumentAttribute[] = [];
+        const flags: ArgumentFlag[] = [];
+
+        this.args.forEach((value, index) => {
+            if (value.startsWith("-") && !this.isFlag(value)) {
+                for (let i = index; i < this.args.length; i++) {
+                    if (!this.isFlag(value)) {
+                        attrs.push(new ArgumentAttribute(value, this.args[i]));
+                        break;
+                    }
+                }
+            } else if (this.isFlag(value)) {
+                flags.push(new ArgumentFlag(value));
+            }
+        });
+
+        return new ArgumentHandler(flags, attrs);
+    }
+
+    private isFlag(x: string): boolean {
+        return x.startsWith("--");
+    }
+
+    private printGlobalHelp() {
+        // TODO print global help;
+    }
+
+    private printCommandNotFound() {
+        // TODO print command not found;
+    }
+}
